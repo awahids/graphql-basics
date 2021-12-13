e696c81bedd44a8bad363d6edcf89e31b108c91b
awahids <awahid.safhadi@gmail.com>
2021-12-13 21:52:45 +0800

    + config database local

--- config/db.js
+++ config/db.js
@@ -3,7 +3,8 @@ require("dotenv").config();
 
 module.exports = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {});
+    // await mongoose.connect(process.env.MONGO_URI, {});
+    await mongoose.connect(process.env.DB_LOCAL, {});
     console.log("MongoDB are Connected");
   } catch (err) {
     console.log(err);
